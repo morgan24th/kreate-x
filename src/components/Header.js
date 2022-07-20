d794f704bd596689b3d7f82d78fc8d87667631f2
morgan24th <sarpong.prince28@gmail.com>
2022-07-19 21:12:41 -0400

    setting up

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-fuchsia-600'>Logo Comes Here</h1>
+    </div>
+  )
+}
+
+export default Header
